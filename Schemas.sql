4788cf4bb3c075fe5413c0cbe4ddda273adde862
Muhammed Sajil K <mynameissajil@gmail.com>
2026-03-09 21:44:28 +0530

    Update Schemas.sql

--- Schemas.sql
+++ Schemas.sql
@@ -1,20 +1,24 @@
 -- SCHEMAS of Netflix
-
 DROP TABLE IF EXISTS netflix;
 CREATE TABLE netflix
 (
-	show_id	VARCHAR(5),
-	type    VARCHAR(10),
-	title	VARCHAR(250),
-	director VARCHAR(550),
-	casts	VARCHAR(1050),
-	country	VARCHAR(550),
-	date_added	VARCHAR(55),
-	release_year	INT,
-	rating	VARCHAR(15),
-	duration	VARCHAR(15),
-	listed_in	VARCHAR(250),
-	description VARCHAR(550)
+    show_id      VARCHAR(5),
+    type         VARCHAR(10),
+    title        VARCHAR(250),
+    director     VARCHAR(550),
+    casts        VARCHAR(1050),
+    country      VARCHAR(550),
+    date_added   VARCHAR(55),
+    release_year INT,
+    rating       VARCHAR(15),
+    duration     VARCHAR(15),
+    listed_in    VARCHAR(250),
+    description  VARCHAR(550)
 );
+select * from netflix;
 
-SELECT * FROM netflix;
+select count(*) as total_count
+from netflix;
+
+SELECT DISTINCT type
+from netflix;
